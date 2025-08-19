90dbf136f39301fbcc9c61d741173049042e8077
jasonleexc <e1398491@u.nus.edu>
2025-08-19 22:37:12 +0800

    Level-0

---
+++ src/main/java/Borat.java
@@ -0,0 +1,17 @@
+
+public class Borat {
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Borat");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
