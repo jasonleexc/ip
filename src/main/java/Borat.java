0071a1d9b6755cc3300b22711026805cfa29f0bd
jasonleexc <e1398491@u.nus.edu>
2025-08-19 22:46:25 +0800

    Level-1

--- src/main/java/Borat.java
+++ src/main/java/Borat.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
 
 public class Borat {
 
     public static void main(String[] args) {
         greet();
+        echo();
         exit();
     }
 
     private static void greet() {
         System.out.println("Hello! I'm Borat");
         System.out.println("What can I do for you?");
+    }
+
+    private static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = sc.nextLine().trim();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(command);
+        }
+
+        sc.close();
     }
 
     private static void exit() {
