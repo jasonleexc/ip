b43e6201c737c29db2a601f21a0da8b20581b924
jasonleexc <e1398491@u.nus.edu>
2025-09-02 15:45:20 +0800

    Build: Package Borat as executable JAR

--- src/test/java/borat/task/TaskTest.java
+++ src/test/java/borat/task/TaskTest.java
@@ -23,7 +23,7 @@ public class TaskTest {
         assertNotNull(deadline);
         assertNotNull(event);
         
-        // Test that tasks are created with correct descriptions
+        // test that tasks are created with correct descriptions
         assertTrue(todo.toString().contains("Test todo task"));
         assertTrue(deadline.toString().contains("Test deadline task"));
         assertTrue(event.toString().contains("Test event task"));
@@ -31,7 +31,7 @@ public class TaskTest {
 
     @Test
     void testTaskInitialState() {
-        // Test that tasks start as not done
+        // test that tasks start as unmarked
         assertTrue(todo.toString().contains("[ ]"));
         assertTrue(deadline.toString().contains("[ ]"));
         assertTrue(event.toString().contains("[ ]"));
@@ -55,16 +55,15 @@ public class TaskTest {
 
     @Test
     void testToString() {
-        // Test unmarked tasks
         assertEquals("[T][ ] Test todo task", todo.toString());
         assertEquals("[D][ ] Test deadline task (by: Dec 31 2023 23:59)", deadline.toString());
         assertEquals("[E][ ] Test event task (from: Dec 31 2023 20:00 to: Dec 31 2023 22:00)", event.toString());
         
-        // Test marked tasks
         todo.setDone(true);
         deadline.setDone(true);
         event.setDone(true);
-        
+
+        // test that unmarked tasks can be marked as done
         assertEquals("[T][X] Test todo task", todo.toString());
         assertEquals("[D][X] Test deadline task (by: Dec 31 2023 23:59)", deadline.toString());
         assertEquals("[E][X] Test event task (from: Dec 31 2023 20:00 to: Dec 31 2023 22:00)", event.toString());
@@ -72,16 +71,15 @@ public class TaskTest {
 
     @Test
     void testToFileString() {
-        // Test unmarked tasks
         assertEquals("T | 0 | Test todo task", todo.toFileString());
         assertEquals("D | 0 | Test deadline task | Dec 31 2023 23:59", deadline.toFileString());
         assertEquals("E | 0 | Test event task | Dec 31 2023 20:00 | Dec 31 2023 22:00", event.toFileString());
         
-        // Test marked tasks
         todo.setDone(true);
         deadline.setDone(true);
         event.setDone(true);
-        
+
+        // test that unmarked tasks can be marked as done
         assertEquals("T | 1 | Test todo task", todo.toFileString());
         assertEquals("D | 1 | Test deadline task | Dec 31 2023 23:59", deadline.toFileString());
         assertEquals("E | 1 | Test event task | Dec 31 2023 20:00 | Dec 31 2023 22:00", event.toFileString());
@@ -91,18 +89,5 @@ public class TaskTest {
     void testEmptyDescription() {
         ToDo emptyTodo = new ToDo("");
         assertEquals("[T][ ] ", emptyTodo.toString());
-    }
-
-    @Test
-    void testSpecialCharactersInDescription() {
-        ToDo specialTodo = new ToDo("Task with special chars: !@#$%^&*()");
-        assertTrue(specialTodo.toString().contains("Task with special chars: !@#$%^&*()"));
-    }
-
-    @Test
-    void testLongDescription() {
-        String longDesc = "This is a very long description that contains many words and should be handled properly by the task system without any issues or truncation";
-        ToDo longTodo = new ToDo(longDesc);
-        assertTrue(longTodo.toString().contains(longDesc));
     }
 }
